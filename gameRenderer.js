7da55a532066e40ede24d840a5eaa6dad30ce51d
Alexander Eichhorn <alex@fl1rt.net>
2020-02-01 18:21:42 +0100

    field naming

--- gameRenderer.js
+++ gameRenderer.js
@@ -12,7 +12,7 @@ function NewGameRenderer () {
     self.Level.forEach((rows, x) => {
       rows.forEach((v, y) => {
         const field = document.getElementById('f_' + x + '_' + y);
-        field.innerHTML = v;
+        field.innerHTML = '<img scr="field_' + v + '.svg">';
       });
     });
   };
