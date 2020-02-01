fb958050dfee54b89afa7ee84216d766a2212161
Alexander Eichhorn <alex@fl1rt.net>
2020-02-01 18:58:40 +0100

    ahrg

--- gameRenderer.js
+++ gameRenderer.js
@@ -19,7 +19,7 @@ function NewGameRenderer () {
 
   self.Render = function (Robo) {
     const field = document.getElementById('f_' + Robo.x + '_' + Robo.y);
-    field.innerHTML = '<img src="media/field_R.png">';
+    field.innerHTML = '<img src="media/robot_' + Robo.d + '.png">';
     // Packe Robo an robo.x & robo.y mit Richtung robo.r
   };
 
